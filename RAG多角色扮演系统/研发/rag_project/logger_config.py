28f07b186679b65257306582f2819dfea88d65ce
sheng657 <sheng657@users.noreply.github.com>
2026-05-29 13:32:56 +0800

    删除RAG多角色扮演系统文件夹

--- RAG多角色扮演系统/研发/rag_project/logger_config.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# logger_config.py日志配置
-import logging
-
-def setup_logger(name=__name__):
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-    return logging.getLogger(name)
-
-logger = setup_logger()
